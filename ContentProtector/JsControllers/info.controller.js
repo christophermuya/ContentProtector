71a9f8e4bb9a8aa4c694cf0fc6f8f312b9ba932f
jake williamson <jake@koben.com.au>
2023-07-18 11:25:50 +1000

    deleted original code

--- ContentProtector/JsControllers/info.controller.js
+++
@@ -1,4 +0,0 @@
-﻿angular.module("umbraco").controller("cp.info.controller", function ($http, entityResource, contentResource, editorService, $route, cpResource, usersResource, userService, mpPropertyEditor, notificationsService) {
-    var vm = this;
-    vm.Name = "cp.info";
-});
